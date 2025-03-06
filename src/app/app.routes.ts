cab866f5a417eac2c4e0d2f1bcb5244f1e179840
Pavlo <pppp_84@ukr.net>
2025-03-07 01:13:19 +0200

    created messages component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import {PersonGridComponent} from './person-grid/person-grid.component';
 import {PersonAddEditComponent} from './person-add-edit/person-add-edit.component';
+import {MessagesComponent} from './messages/messages.component';
 
 export const routes: Routes = [
   {path: 'add-person', component: PersonAddEditComponent},
-  {path: 'dashboard', component: PersonGridComponent}
+  {path: 'dashboard', component: PersonGridComponent},
+  {path: 'messages', component: MessagesComponent}
 ];
---
+++ src/app/messages/messages.component.ts
@@ -0,0 +1,72 @@
+import { Component } from '@angular/core';
+import  Stomp from 'stompjs';
+import SockJS from 'sockjs-client';
+
+
+@Component({
+  selector: 'app-messages',
+  imports: [],
+  templateUrl: './messages.component.html',
+  styleUrl: './messages.component.css'
+})
+export class MessagesComponent {
+
+  stompClient:any = null;
+
+  constructor() {
+    // this.connect()
+  }
+
+  ngOnInit() {
+    this.connect()
+  }
+
+    connect() {
+    // const Stomp = require("stompjs");
+    //   var Stomp:Stomp;
+
+      // var SockJS = require("sockjs-client");
+    let ws: any = new SockJS("http://localhost:8080/ws");
+    this.stompClient = Stomp.over(ws);
+    this.stompClient.connect({},
+    //   () => {
+    //   console.log('conncted')
+    // }
+    this.onConnected
+    , () => {
+      console.log('error')
+    });
+  };
+    onConnected = () => {
+    console.log("connected");
+
+    this.stompClient.subscribe(
+      "/topic/notification",
+      // "/user/" + currentUser.id + "/queue/messages",
+      // onMessageReceived
+        (msg: any) => {
+        console.log(msg)
+        }
+    );
+  };
+
+  sendMessage(msg: any) {
+    if (msg.trim() !== "") {
+      const message = {
+        // senderId: currentUser.id,
+        // recipientId: activeContact.id,
+        // senderName: currentUser.name,
+        // recipientName: activeContact.name,
+        // content: msg,
+        // timestamp: new Date(),
+        title: 'title1'
+      };
+
+      console.log("before seending")
+      this.stompClient.send("/app/note", {}, JSON.stringify(message));
+      console.log("after sending")
+    }
+  };
+
+
+}
